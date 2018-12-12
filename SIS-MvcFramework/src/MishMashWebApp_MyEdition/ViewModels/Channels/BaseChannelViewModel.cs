357aff3c67065bedf778394d974d12f995cc59d9
ivanyanev <ivan.yanevmail@gmail.com>
2018-12-12 19:08:36 +0200

    non versioned files added

---
+++ SIS-MvcFramework/src/MishMashWebApp_MyEdition/ViewModels/Channels/BaseChannelViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MishMashWebApp_MyEdition.Model;
+
+namespace MishMashWebApp_MyEdition.ViewModels.Channels
+{
+    public class BaseChannelViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ChannelType Type { get; set; }
+
+        public int FollowersCount { get; set; }
+    }
+}
---
+++ SIS-MvcFramework/src/MishMashWebApp_MyEdition/ViewModels/Home/LoggedInIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MishMashWebApp_MyEdition.Controllers;
+using MishMashWebApp_MyEdition.ViewModels.Channels;
+using System.Collections.Generic;
+
+namespace MishMashWebApp_MyEdition.ViewModels.Home
+{
+    public class LoggedInIndexViewModel : BaseController
+    {
+        public string UserRole { get; set; }
+
+        public IEnumerable<BaseChannelViewModel> YourChannels { get; set; }
+
+        public IEnumerable<BaseChannelViewModel> SuggestedChannels { get; set; }
+
+        public IEnumerable<BaseChannelViewModel> SeeOtherChannels { get; set; }
+    }
+}
